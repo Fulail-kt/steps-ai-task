0ed25ad60e15d02eee7aa169d5248987e6b03951
fulail-kt <muhamedfulail77@gmail.com>
2024-07-19 22:28:54 +0530

    ts removed

--- client/src/app/(doctor)/patients/page.jsx
+++ client/src/app/(doctor)/patients/page.jsx
@@ -1,6 +1,3 @@
-
-import { jwtDecode } from 'jwt-decode';
-import React, { useEffect, useState } from 'react';
 import Patients from '../../../components/pages/doctor/patients'
 
 const Page = () => {
